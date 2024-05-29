c635698515f67e33b4299c342a28cdc0b21411a9
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2024-05-29 22:07:14 +0500

    Update and rename pg15_20.java to Ifelse_pg.java

--- Ifelse_pg.java
+++ Ifelse_pg.java
@@ -1,14 +1,15 @@
+/*This Java program consists of commented-out code blocks for various challenges */
 import java.util.Scanner;
-public class pg15_20 {
+public class Ifelse_pg {
     public static void main(String[] args) {
         Scanner sk= new Scanner(System.in);
-     //---challenge 0015
+     //---challenge B:Prompt user to enter a number and classify it as positive, negative, or neither.
 //        System.out.print("Enter any number: ");
 //        int a=sk.nextInt();
 //        if(a<0) System.out.println("Negative number");
 //        else if (a>0) System.out.println("Positive number");
 //        else System.out.println("It is Neither");
-      // challenge 0016
+      // challenge B:Prompt user to enter a number and determine if it's even or odd.
 //        System.out.print("Enter any number: ");
 //        int eo=sk.nextInt();
 //
@@ -17,7 +18,7 @@ public class pg15_20 {
 //        }
 //        else System.out.println("It is an odd ");
 //    }
-        //challenge 0017
+        //challenge C:Prompt user to enter three numbers and identify if they are equal or find the largest among them.
 //        System.out.println("Enter Any three numbers");
 //        System.out.print("Number one: ");
 //        int n1=sk.nextInt();
@@ -35,15 +36,13 @@ public class pg15_20 {
 //        if(n3>largest){
 //            if(n3>n2){System.out.println(n3+" is the largest number ");}
 //            else System.out.println(n2+" is the largest number ");}
-    // challenge 0018
+    // challenge D:Prompt user to enter a year and determine if it's a leap year.
 //        System.out.println("Leap year Identifying Machine ");
 //        System.out.print("Enter the year to be checked: ");
 //        int year= sk.nextInt();
 //        // from ChatGPT
 //        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {System.out.println(year+"  is a leap year ");}
 //        else {System.out.println(year+"  is not  a leap year ");}
-     //challenge 0019: Grading System
-        //challenge 0020: age categorization
-
+    
     }
 }
