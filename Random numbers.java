05d0d28ca490a5dd8155697a14669aced2c53ac8
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2024-05-29 20:27:55 +0500

    Update Random numbers.java

--- Random numbers.java
+++ Random numbers.java
@@ -1,14 +1,13 @@
-import java.util.Random;
+#This Java program generates and prints a random integer between 1 and 100.
 
+import java.util.Random;
 public class first {
     public static void main(String[] args) {
-        // Create a Random object to generate random numbers
+        
         Random random = new Random();
 
-        // Generate a random integer between 1 and 100 (inclusive)
         int randomNumber = random.nextInt(100) + 1;
 
-        // Display the generated random number
-        System.out.println("Random Number: " + randomNumber);
+        System.out.println("Random Numberm is : " + randomNumber);
     }
 }
