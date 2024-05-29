d25706e4ec8f4e7f4d026a31ac495b4abea7bfa4
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2024-05-29 21:45:26 +0500

    Update and rename Set_04_Q4.java to dayname_wrt_num.java

--- dayname_wrt_num.java
+++ dayname_wrt_num.java
@@ -1,9 +1,10 @@
+/*This Java program prompts the user to enter a day number and then prints the corresponding day of the week.*/
 import java.util.Scanner;
-public class Set_04_Q4 {
+public class dayname_wrt_num {
     public static void main(String[] args) {
         Scanner take = new Scanner(System.in);
+        System.out.print("Enter the day number: ");
         int day = take.nextInt();
-
         switch (day) {
             case 1:
                 System.out.println("It's Monday");
