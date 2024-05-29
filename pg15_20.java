02d19c5de5decaecbb2d933a6721c0f9a5c169dc
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2024-05-29 21:47:44 +0500

    Add files via upload

---
+++ pg15_20.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+public class pg15_20 {
+    public static void main(String[] args) {
+        Scanner sk= new Scanner(System.in);
+     //---challenge 0015
+//        System.out.print("Enter any number: ");
+//        int a=sk.nextInt();
+//        if(a<0) System.out.println("Negative number");
+//        else if (a>0) System.out.println("Positive number");
+//        else System.out.println("It is Neither");
+      // challenge 0016
+//        System.out.print("Enter any number: ");
+//        int eo=sk.nextInt();
+//
+//        if(eo%2==0){
+//            System.out.println("It is an even number");
+//        }
+//        else System.out.println("It is an odd ");
+//    }
+        //challenge 0017
+//        System.out.println("Enter Any three numbers");
+//        System.out.print("Number one: ");
+//        int n1=sk.nextInt();
+//        System.out.print("Number two: ");
+//        int n2=sk.nextInt();
+//        System.out.print("Number three: ");
+//        int n3=sk.nextInt();
+//        if(n1==n2&&n2==n3){System.out.print("All numbers given are equal");}
+//        int largest=n1;
+//        if(n2>largest){
+//            if(n2>n3){
+//                System.out.println(n2+" is the largest number ");}
+//            else System.out.println(n3+" is the largest number ");}
+//        else
+//        if(n3>largest){
+//            if(n3>n2){System.out.println(n3+" is the largest number ");}
+//            else System.out.println(n2+" is the largest number ");}
+    // challenge 0018
+//        System.out.println("Leap year Identifying Machine ");
+//        System.out.print("Enter the year to be checked: ");
+//        int year= sk.nextInt();
+//        // from ChatGPT
+//        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {System.out.println(year+"  is a leap year ");}
+//        else {System.out.println(year+"  is not  a leap year ");}
+     //challenge 0019: Grading System
+        //challenge 0020: age categorization
+
+    }
+}
