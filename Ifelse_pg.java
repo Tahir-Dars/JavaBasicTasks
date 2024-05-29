7660ade5255dc80fc496fa571931c008e55c73bb
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2024-05-29 22:09:35 +0500

    Update Ifelse_pg.java

--- Ifelse_pg.java
+++ Ifelse_pg.java
@@ -1,15 +1,16 @@
 /*This Java program consists of commented-out code blocks for various challenges */
+/*Uncomment the part you want to use */
 import java.util.Scanner;
 public class Ifelse_pg {
     public static void main(String[] args) {
         Scanner sk= new Scanner(System.in);
-     //---challenge B:Prompt user to enter a number and classify it as positive, negative, or neither.
+    /*challenge B:Prompt user to enter a number and classify it as positive, negative, or neither.*/
 //        System.out.print("Enter any number: ");
 //        int a=sk.nextInt();
 //        if(a<0) System.out.println("Negative number");
 //        else if (a>0) System.out.println("Positive number");
 //        else System.out.println("It is Neither");
-      // challenge B:Prompt user to enter a number and determine if it's even or odd.
+      /* challenge B:Prompt user to enter a number and determine if it's even or odd.8/
 //        System.out.print("Enter any number: ");
 //        int eo=sk.nextInt();
 //
@@ -18,7 +19,8 @@ public class Ifelse_pg {
 //        }
 //        else System.out.println("It is an odd ");
 //    }
-        //challenge C:Prompt user to enter three numbers and identify if they are equal or find the largest among them.
+        /*challenge C:Prompt user to enter three numbers 
+        and identify if they are equal or find the largest among them.*/
 //        System.out.println("Enter Any three numbers");
 //        System.out.print("Number one: ");
 //        int n1=sk.nextInt();
@@ -36,7 +38,7 @@ public class Ifelse_pg {
 //        if(n3>largest){
 //            if(n3>n2){System.out.println(n3+" is the largest number ");}
 //            else System.out.println(n2+" is the largest number ");}
-    // challenge D:Prompt user to enter a year and determine if it's a leap year.
+    /*challenge D:Prompt user to enter a year and determine if it's a leap year.*/
 //        System.out.println("Leap year Identifying Machine ");
 //        System.out.print("Enter the year to be checked: ");
 //        int year= sk.nextInt();
