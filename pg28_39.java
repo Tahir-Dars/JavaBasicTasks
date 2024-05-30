b6a68a6a0460b5c3304832dbff2effa90bdb1644
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2024-05-30 13:08:55 +0500

    Add files via upload

---
+++ pg28_39.java
@@ -0,0 +1,90 @@
+import javax.sound.sampled.ReverbType;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class pg28_39 {
+    public static void main(String[] args) {
+        Scanner ta=new Scanner(System.in);
+        // challenge 0028
+//        System.out.print("Write a number for making its Table: ");
+//        int p=ta.nextInt();
+//        System.out.print(" till How much: ");
+//        int k= ta.nextInt();
+//        for(int i=1; i<=k;i++){
+//            System.out.println(p+" X "+i+" = "+(p*i));
+//        }
+       //chqallenge 0029
+//        System.out.println("A program to sum all odd numbers between 1 and specified imput number ");
+//        System.out.print("Enter a number: ");
+//        int k=ta.nextInt();
+//        int oddSum=1;
+//        for(int i=2;i<=k;i++){
+//            if(i%2!=0){
+//                oddSum+=i;
+//            }
+//        }
+//        System.out.print(oddSum);
+        //challenge 0030
+//        System.out.print("Enter a number to find factorial of: ");
+//        int numb= ta.nextInt();
+//        System.out.println("The Factorial of "+numb+"  is : "+factorial(numb));
+        //challenge 0031
+//        System.out.println("A Program to calculate the sum of digits in an integer ");
+//        System.out.print("Enter a number: ");
+//        int Entry=ta.nextInt();
+//        int length= (int) Math.log10(Entry)+1;
+//        int digiSum=0;
+//        for(int i=1;i<=length;i++){
+//            digiSum+=Entry%10;
+//            Entry=Entry/10;
+//        }
+//        System.out.println(" The Sum is: "+ digiSum);
+      // challenge 0032
+//        System.out.println("Enter two numbers for finding LCM");
+//        int HCF=1;
+//        int max;
+//        System.out.print("Number 001: ");
+//        int a=ta.nextInt();
+//        System.out.print("Number 002: ");
+//        int b=ta.nextInt();
+//        if(a>b){max=b;}
+//        else {max=a;}
+//        for(int i=1;i<=max;i++) {if (a % i == 0 && b % i == 0) {HCF = i;}}
+//        System.out.println("THE HCF OF "+a+" and "+b+" is :"+HCF);
+        //challenge 0033
+//        System.out.println("Enter two numbers for finding LCM");
+//        System.out.print("Number 001: ");
+//        int one1= ta.nextInt();
+//        System.out.print("Number 002: ");
+//        int one2= ta.nextInt();
+//        float LCM;
+//        for (int i=1;i<=one2;i++){
+//            LCM=one1*i;
+//           if(LCM%one2==0){
+//               System.out.println("LCM of the numers "+one1+" and "+one2+" is "+LCM);
+//                 return;
+//           }
+//        }
+        //challenge 0034
+//        System.out.print("Enter a number to chack if prime:  ");
+//        int primeornot= ta.nextInt();
+//        for(int i=2;i<primeornot;i++){
+//            if(primeornot%i==0){
+//                System.out.println("This is not Prime");
+//                return;
+//            }
+//            else {
+//                System.out.println("This is Prime ");
+//                return;
+//            }
+//        }
+        //cahllenge 0035 code on the copy
+
+        System.out.print("Enter a number: ");
+        int number=ta.nextInt();
+        for (int i=1;i<=lengthOf(number))
+
+
+
+    }
+
+}
