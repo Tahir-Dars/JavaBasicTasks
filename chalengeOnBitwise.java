635b7d8f5502b3daebd08a3918006595c69a6976
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2024-05-30 14:23:05 +0500

    Add files via upload

---
+++ chalengeOnBitwise.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class chalengeOnBitwise {
+    public static void main(String[] args) {
+        Scanner tool= new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int bitwise= tool.nextInt();
+        System.out.print("Enter the second number: ");
+        int bitwise2= tool.nextInt();
+        System.out.println("Their AND operation will result in : "+ (bitwise2&bitwise));
+        System.out.println("Their OR operation will result in : "+ (bitwise2|bitwise));
+        System.out.println("Their XOR operation will result in : "+ (bitwise2^bitwise));
+        System.out.println("Their NOT operation on Number 1 is : "+ (~bitwise));
+        System.out.println("Their NOT operation on Number 2 is : "+ (~bitwise2));
+
+
+    }
+}
