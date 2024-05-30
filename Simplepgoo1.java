8cbbc382dc4d3381f8450e5d65045072d72af7ee
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2024-05-30 14:02:08 +0500

    Update and rename challenges_all.java to Simplepgoo1.java

--- Simplepgoo1.java
+++ Simplepgoo1.java
@@ -1,20 +1,26 @@
 import java.util.Scanner;
 
-public class challenges_all {
+public class Simplepg001 {
     public static void main(String[] args) {
         Scanner sk= new Scanner(System.in);
 
-        // challenge 001
-        /*System.out.print("Write you name, please: ");
-        String name= sk.nextLine();
-        System.out.print("Welcome Dear"+name+",to Our Academy ");*/
-        //Challenge 002
+        // challenge a:Ask for the user's name and welcome them to an academy
+
+        // System.out.print("Write you name, please: ");
+        // String name= sk.nextLine();
+        // System.out.print("Welcome Dear"+name+",to Our Academy ");
+
+        //Challenge b :Prompt the user to enter two numbers and print their sum.
+
         /*System.out.println("Welcome to MMD Calculator\n--Enter two numbers\nNumber one:");
              int num1= sk.nextInt();
         System.out.print("Number two:");
             int num2= sk.nextInt();
         System.out.println("Their sum is: "+(num1+num2));*/
-        //----Challenge 003
+
+        //----Challenge c:Prompt the user to enter two numbers and perform basic 
+        //arithmetic operations (addition, subtraction, multiplication, division, modulus).
+
 //        System.out.println("Enter First number");
 //        int   a= sk.nextInt();
 //        System.out.println("Enter seconf number");
@@ -24,26 +30,34 @@ public class challenges_all {
 //        System.out.println("Their multiplication is : "+(a*b));
 //        System.out.println("Their division is : "+(a/b));
 //        System.out.println("Their modulus  is : "+(a%b));
-        //----------challenge 004
+
+        //----------challenge d:Prompt the user to enter two decimal numbers and print their product.
+
 //        System.out.print("Enter a decimal number:");
 //        float f1= sk.nextFloat();
 //        System.out.print("Enter second decimal number:");
 //        float f2= sk.nextFloat();
 //        System.out.println("Their product is : "+f1*f2 );
-        //----------challenge 005
+
+        //----------challenge e:Prompt the user to enter the length and breadth of a rectangle and calculate its perimeter.
+
 //        System.out.print("Note here the length of anyside pf rectangle");
 //        float l=sk.nextFloat();
 //        System.out.println("Now note the length of its any adjacent side");
 //        float b=sk.nextFloat();
 //        System.out.println("The perimeter of the rectangle is"+(l+b)*2);
-        //challenge 006
+
+        //challenge f:Prompt the user to enter the base and height of a triangle and calculate its area.
+
 //        System.out.println("Calculating the area of the triangle");
 //        System.out.print("Enter the base of the triangle: ");
 //        float b=sk.nextFloat();
 //        System.out.print("Enter the height of the triangle: ");
 //        float h=sk.nextFloat();
 //        System.out.println("Area of the Triangle is : "+(b*h)/2);
-        //----challenge 007
+
+        //----challenge g:Prompt the user to enter the principal amount, rate, and time period, and calculate the simple interest.
+
 //        System.out.println("Calculating the simple and compound intrest calculator ");
 //        System.out.print("Enter the Principal amount: ");
 //        float p=sk.nextFloat();
@@ -52,14 +66,6 @@ public class challenges_all {
 //        System.out.print("Enter the time period(in years): ");
 //        float t=sk.nextFloat();
 //        System.out.println("The Simple Intrest in numbers:"+(p*r*t)/100);
-//        ------challenge 008
-//      float f=sk.nextFloat();
-//       System.out.print("Enter the temperature in Farenheit: ");
-//     System.out.println("The same temperature in Celcius is:"+((f-32)*(5/9f)));
-
-
-
-
 
 
     }
