886d56db76481c199bb3ac7f45139f34ec6ecf5d
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2024-05-30 14:00:57 +0500

    Add files via upload

---
+++ challenges_all.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+
+public class challenges_all {
+    public static void main(String[] args) {
+        Scanner sk= new Scanner(System.in);
+
+        // challenge 001
+        /*System.out.print("Write you name, please: ");
+        String name= sk.nextLine();
+        System.out.print("Welcome Dear"+name+",to Our Academy ");*/
+        //Challenge 002
+        /*System.out.println("Welcome to MMD Calculator\n--Enter two numbers\nNumber one:");
+             int num1= sk.nextInt();
+        System.out.print("Number two:");
+            int num2= sk.nextInt();
+        System.out.println("Their sum is: "+(num1+num2));*/
+        //----Challenge 003
+//        System.out.println("Enter First number");
+//        int   a= sk.nextInt();
+//        System.out.println("Enter seconf number");
+//       int  b= sk.nextInt();
+//        System.out.println("Their addition is : "+(a+b));
+//        System.out.println("Their subtraction is : "+(a-b));
+//        System.out.println("Their multiplication is : "+(a*b));
+//        System.out.println("Their division is : "+(a/b));
+//        System.out.println("Their modulus  is : "+(a%b));
+        //----------challenge 004
+//        System.out.print("Enter a decimal number:");
+//        float f1= sk.nextFloat();
+//        System.out.print("Enter second decimal number:");
+//        float f2= sk.nextFloat();
+//        System.out.println("Their product is : "+f1*f2 );
+        //----------challenge 005
+//        System.out.print("Note here the length of anyside pf rectangle");
+//        float l=sk.nextFloat();
+//        System.out.println("Now note the length of its any adjacent side");
+//        float b=sk.nextFloat();
+//        System.out.println("The perimeter of the rectangle is"+(l+b)*2);
+        //challenge 006
+//        System.out.println("Calculating the area of the triangle");
+//        System.out.print("Enter the base of the triangle: ");
+//        float b=sk.nextFloat();
+//        System.out.print("Enter the height of the triangle: ");
+//        float h=sk.nextFloat();
+//        System.out.println("Area of the Triangle is : "+(b*h)/2);
+        //----challenge 007
+//        System.out.println("Calculating the simple and compound intrest calculator ");
+//        System.out.print("Enter the Principal amount: ");
+//        float p=sk.nextFloat();
+//        System.out.print("Enter the rate per time period: ");
+//        float r=sk.nextFloat();
+//        System.out.print("Enter the time period(in years): ");
+//        float t=sk.nextFloat();
+//        System.out.println("The Simple Intrest in numbers:"+(p*r*t)/100);
+//        ------challenge 008
+//      float f=sk.nextFloat();
+//       System.out.print("Enter the temperature in Farenheit: ");
+//     System.out.println("The same temperature in Celcius is:"+((f-32)*(5/9f)));
+
+
+
+
+
+
+    }
+}
