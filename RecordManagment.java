6e93324fa3b938cc148f51249041f516420a651d
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2024-05-30 15:40:55 +0500

    Update RecordManagment.java

--- RecordManagment.java
+++ RecordManagment.java
@@ -1,29 +1,40 @@
+/*The functionality of this Java program is to prompt the user to input a name and a year, create a record object with these
+values, validate the year within a specified range (1996-2023), and add the record to a list if it meets the criteria.*/
 import java.util.ArrayList;
-class Record{
+import java.util.Scanner;
+
+class Record {
     int year;
     String name;
-    // ArrayList<Record> arr = new ArrayList<>();
     ArrayList<Record> arr = new ArrayList<>();
-    void addrecord( Record r) throws Exception {
-        if (r.year < 1996 || r.year > 2023) {
 
-            throw new Exception("invalid year");
+    void addRecord(Record r) throws Exception {
+        if (r.year < 1996 || r.year > 2023) {
+            throw new Exception("Invalid year");
         } else {
             arr.add(r);
-        } }
-    void printrecord(){
-        for(Record r:arr){
+        }
+    }
+
+    void printRecord() {
+        for (Record r : arr) {
             System.out.println(r);
         }
-    } }
+    }
+}
 
-
-public class RecordManagment{
-    public static void main(String[] args) throws Exception
-    {
+public class RecordManagement {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        
         Record r = new Record();
-        r.name="ibad";
-        r.year=1995;
-        r.addrecord(r);
+        
+        System.out.print("Enter the name: ");
+        r.name = scanner.nextLine();
+        
+        System.out.print("Enter the year: ");
+        r.year = scanner.nextInt();
+        
+        r.addRecord(r);
     }
 }
