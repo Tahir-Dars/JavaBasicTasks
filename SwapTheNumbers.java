1c647a71ffc766210e901bc9a60cbf3bcc261082
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2024-06-03 00:53:37 +0500

    Update SwapTheNumbers.java

--- SwapTheNumbers.java
+++ SwapTheNumbers.java
@@ -1,4 +1,4 @@
-# This Java program swaps the values of two integers without using a temporary variable
+// This Java program swaps the values of two integers without using a temporary variable
 import java.util.Scanner;
 public class SwapTheNumbers {
     public static void main(String[] args){
